c5ac60ea3e10ad0103e80e5054ae8b4d55b21ce6
Brayan Santos <santos.a.brayan97@gmail.com>
2021-09-27 20:49:55 -0400

    server modified

--- server.js
+++ server.js
@@ -1,42 +1,52 @@
-const http = require('http')
-const express = require("express");
-const socketio = require("socket.io");
+// const http = require('http')
+// const express = require("express");
+// const socketio = require("socket.io");
 const PORT = process.env.PORT || 3000;
-const cors = require('cors');
-const app = express();
-const httpServer = http.Server(app)
-const io = socketio(httpServer);
+// const cors = require('cors');
+// const app = express();
+// const httpServer = http.Server(app)
+// const io = socketio(httpServer);
+//
+// app.use(cors());
+// app.use(express.json());
+//
+// let rooms = [];
+// let usernames = [];
+//
+// io.on('connection', function(socket){
+//
+//   socket.on("join", function(room, username){
+//     if (username != ""){
+//       rooms[socket.id] = room;
+//       usernames[socket.id] = username;
+//       socket.leaveAll();
+//       socket.join(room);
+//       io.in(room).emit("recieve", "Server : " + username + " has entered the chat.");
+//       socket.emit("join", room);
+//       console.log(room);
+//       console.log(username);
+//     }
+//   })
+//
+//   socket.on("send", function(message){
+//     io.in(rooms[socket.id]).emit("recieve", usernames[socket.id] +" : " + message);
+//     console.log(usernames);
+//   });
+//
+//   socket.on("recieve", function(message){
+//     socket.emit("recieve", message);
+//     console.log(message);
+//   });
+// });
+//
+// httpServer.listen(PORT);
 
-app.use(cors());
-app.use(express.json());
+const socketio = require("socket.io");
+const io = socketio();
 
-let rooms = [];
-let usernames = [];
-
-io.on('connection', function(socket){
-
-  socket.on("join", function(room, username){
-    if (username != ""){
-      rooms[socket.id] = room;
-      usernames[socket.id] = username;
-      socket.leaveAll();
-      socket.join(room);
-      io.in(room).emit("recieve", "Server : " + username + " has entered the chat.");
-      socket.emit("join", room);
-      console.log(room);
-      console.log(username);
-    }
-  })
-
-  socket.on("send", function(message){
-    io.in(rooms[socket.id]).emit("recieve", usernames[socket.id] +" : " + message);
-    console.log(usernames);
-  });
-
-  socket.on("recieve", function(message){
-    socket.emit("recieve", message);
-    console.log(message);
+io.on('connection', function(socket) {
+  socket.on('send', function(message){
+      io.emit('receive', message);
   });
 });
-
-httpServer.listen(PORT);
+io.listen(3000||PORT);
