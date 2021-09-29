1de049248131f64aadd253c997c96265d39c79d1
James McMorran <jtmcmorran@gmail.com>
2021-09-28 21:26:50 -0400

    aaaaaaaah

--- server.js
+++ server.js
@@ -44,8 +44,10 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const socketio = require("socket.io");
 const io = socketio();
+const httpServer = http.Server(app)
 const cors = require('cors');
 const app = express();
+const httpServer = http.Server(app);
 app.all('/fetch', request, response, next) =>{
   response.header('Access-Control-Allow-Origin', '*');
   next();
